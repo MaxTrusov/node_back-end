9c4ce0e65fd059b97b68e9be7b659ead8b6d4538
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2023-06-30 11:05:32 +0300

    commit-1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,37 @@
-import {analitics} from './modules/analitics.js'
+import express, { Request, Response } from 'express'
+const app = express()
+const port = process.env.PORT || 3000
 
-const message = "Hello NodeJS!"
-console.log(message)
 
-analitics('main.ts')
+
+app.get('/about', (req: Request, res: Response) => {
+   res.end(`<div>
+         <h1>Stroy-city </h1> 
+         
+         <p>Home page</p>
+         <nav>
+            <ul> 
+               <li> <a href="/about">Home page</a> </li>
+               <li> <a href="/services">Services page</a> </li>
+            </ul>
+         </nav>
+            
+      </div>`)
+})
+
+app.get('/services', (req: Request, res: Response) => {
+   res.end(`<div> 
+   <h2>Stroy-city</h2>
+   <h3>строительный портал</h3> 
+   <p>Services page</p>  
+   <nav>
+   <ul> 
+      <li> <a href="/about">Home page</a> </li>
+      <li> <a href="/services">Services page</a> </li>
+   </ul>
+</nav> </div>`)
+})
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${ port }`)
+})
